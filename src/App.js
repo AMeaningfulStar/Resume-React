92c04b01ebbaf9b61d5852ff185c16a3d0e63952
AMeaningfulStar <ghrlfehd123@naver.com>
2022-12-03 19:32:18 +0900

    upload files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './Main.css'; 
+import React from 'react';
+import Header from './components/Header.js';
+
+function App() {
+  return (
+    <div className='layout'>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
